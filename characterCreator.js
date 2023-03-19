52e5917deb59bceff9d9dc8ae81a1a5e0ebb0f72
davidkuhne <davidkuhne@gmail.com>
2023-03-18 21:30:23 -0400

    Add files via upload

---
+++ characterCreator.js
@@ -0,0 +1,45 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "charOptions.xml", false);
+    xhttp.send();
+    PopulateDropDownList(xhttp);//(this);
+}
+function myFunction(xml)
+{
+    var xmlText = xml.responseXML;
+	var xmlDoc;
+	if (window.DOMParser) {
+		parser = new DOMParser();
+		xmlDoc = parser.parseFromString(xmlText, "text/xml");
+	}
+	else //For IE
+	{
+		xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+		xmlDoc.async = false;
+		xmlDoc.loadXML(xml);
+	}
+
+	var classes = xmlDoc.getElementsByTagName("Classes");
+	var sccn = document.getElementById("selectCharClassName");
+
+	for (var i = 0; i < classes.length; i++) {
+		var option = document.createElement("option");
+
+		//Set Customer Name in Text part.
+		option.text = classes[i].getElementsByTagName("Name")[0].textContent;
+
+		//Set Customer CustomerId in Value part.
+		option.value = classes[i].getElementsByTagName("Name")[0].textContent;
+
+		//Add the Option element to DropDownList.
+		sccn.options.add(option);
+	}
+	
+    alert(x);
+}
+
+window.addEventListener("load", function() {
+	loadDoc();
+});
+
+// document.getElementById("content").innerHTML = "whatever";
